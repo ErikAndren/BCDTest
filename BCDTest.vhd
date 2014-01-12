3cf603f9a19c5bafa0a053a3098d2eb46ba979f6
Erik Zachrisson <erik@zachrisson.info>
2014-01-12 22:09:49 +0100

    Update design with new bcd function

--- BCDTest.vhd
+++ BCDTest.vhd
@@ -21,22 +21,19 @@ end entity;
 architecture rtl of BCDTest is
 	constant Freq : positive := 50000000;
 	--
-	signal ClkCnt_N, ClkCnt_D : word(bits(freq)-1 downto 0);
-	signal Tick : bit1;
+	signal ClkCnt_N, ClkCnt_D : word(bits(Freq)-1 downto 0);
 	--
-	signal CntVal_N, CntVal_D : word(8-1 downto 0);
-	signal CntValBcd : word(16-1 downto 0);
+	signal CntVal_N, CntVal_D : word(27-1 downto 0);
+	signal CntValBcd : word(Displays*4-1 downto 0);
 	signal CurSeg : word(4-1 downto 0);
 
 begin
-	ClkCntAsync : process (ClkCnt_D)
+	ClkCntAsync : process (ClkCnt_D, CntVal_D)
 	begin
 		CntVal_N <= CntVal_D;
 		ClkCnt_N <= ClkCnt_D + 1;
-		Tick <= '0';
 		if ClkCnt_D = Freq then
 			ClkCnt_N <= (others => '0');
-			Tick <= '1';
 			CntVal_N <= CntVal_D + 1;
 		end if;
 	end process;
@@ -48,8 +45,9 @@ begin
 			CntVal_D <= CntVal_N;
 		end if;
 	end process;
-	CntValBcd <= to_bcd(CntVal_D(8-1 downto 0));
-	CurSeg    <= ExtractSlice(xt0(16) & CntValBcd, 4, conv_integer(ClkCnt_D(17 downto 15)));
+	--
+	CntValBcd <= ToBcd(CntVal_D, 8);
+	CurSeg    <= ExtractSlice(CntValBcd, 4, conv_integer(ClkCnt_D(17 downto 15)));
 	--
 	Segments  <= BcdArray(conv_integer(CurSeg));
 	Display   <= not SHL(xt0(Displays-1) & '1', ClkCnt_D(17 downto 15));
